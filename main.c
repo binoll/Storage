363f245e42ec4467e6787447fa9d07fbcf9c7cbf
binoll <onionlsfa@gmail.com>
2024-04-09 20:59:50 +0300

    refactor: refactored code

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
+// Copyright 2024 binoll
 #include "libs.h"
 
 int32_t main(int argc, const char* argv[]) {
 	const uint32_t MAX_PATH_SIZE = 255;
 	char path_to_file[MAX_PATH_SIZE];
 	char path_to_storage[MAX_PATH_SIZE];
+	char choice;
 	FILE* file_ptr;
 	FILE* storage_ptr;
-	char choice;
 
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s <file_as_storage> <file_for_save>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 	if (strlen(argv[2]) > MAX_PATH_SIZE || strlen(argv[1]) > MAX_PATH_SIZE) {
-		fprintf(stderr, "[-] Error! Your path must be less than 255 characters!\n");
+		fprintf(stderr, "[-] Error: Your path must be less than 255 characters!\n");
 		return EXIT_FAILURE;
 	}
 	strcpy(path_to_file, argv[2]);
@@ -26,13 +27,16 @@ int32_t main(int argc, const char* argv[]) {
 		file_ptr = fopen(path_to_file, "rb");
 		storage_ptr = fopen(path_to_storage, "r+b");
 		if (!file_ptr || !storage_ptr) {
-			fprintf(stderr, "[-] Error! Failed to open the file(s)!\n");
+			fprintf(stderr, "[-] Error: Failed to open the file(s)!\n");
 			if (file_ptr) fclose(file_ptr);
 			if (storage_ptr) fclose(storage_ptr);
 			return EXIT_FAILURE;
 		}
 
-		scanf(" %c", &choice);
+		if (scanf(" %c", &choice) == 0) {
+			fprintf(stderr, "[-] \n");
+			return -1;
+		}
 
 		switch (choice - '0') {
 			case 0: {
@@ -40,7 +44,7 @@ int32_t main(int argc, const char* argv[]) {
 			}
 			case 1: {
 				if (get_file(fileno(file_ptr), fileno(storage_ptr)) == 0) {
-					fprintf(stdout, "[+] Getting path_to_file\n");
+					fprintf(stdout, "[+] Success: Getting path_to_file.\n");
 				}
 				break;
 			}
@@ -51,7 +55,7 @@ int32_t main(int argc, const char* argv[]) {
 				break;
 			}
 			default: {
-				fprintf(stdout, "[-] Wrong number! Try again!\n");
+				fprintf(stderr, "[-] Error: Wrong number! Try again!\n");
 				break;
 			}
 		}
--- src/save_file.c
+++ src/save_file.c
@@ -1,3 +1,4 @@
+// Copyright 2024 binoll
 #include "../libs.h"
 
 int8_t save_file(int32_t file_fd, int32_t storage_fd) {
@@ -9,26 +10,23 @@ int8_t save_file(int32_t file_fd, int32_t storage_fd) {
 	off_t storage_size, file_size;
 	uint64_t block_size;
 
-	// Получаем информацию о файловой системе хранилища
 	if (fstatvfs(storage_fd, &fs_info) == -1) {
-		perror("[-] Error: Failed to get filesystem information");
+		fprintf(stderr, "[-] Error: Failed to get filesystem information.");
 		return -1;
 	}
 	block_size = fs_info.f_frsize;
 
-	// Получаем размеры файла и хранилища
 	storage_size = lseek(storage_fd, 0, SEEK_END);
 	if (storage_size == -1) {
-		perror("[-] Error: Failed to get size of storage file");
+		fprintf(stderr, "[-] Error: Failed to get size of storage file.");
 		return -1;
 	}
 	file_size = lseek(file_fd, 0, SEEK_END);
 	if (file_size == -1) {
-		perror("[-] Error: Failed to get size of input file");
+		fprintf(stderr, "[-] Error: Failed to get size of input file.");
 		return -1;
 	}
 
-	// Проверяем, достаточно ли места в хранилище для записи файла
 	if (block_size - storage_size < file_size) {
 		fprintf(stderr, "[-] Error: The size of your file(%lu bytes) "
 		                "is larger than the available space in the storage(%lu bytes)!\n",
@@ -36,44 +34,39 @@ int8_t save_file(int32_t file_fd, int32_t storage_fd) {
 		return -1;
 	}
 
-	// Перемещаем указатель в начало файла для чтения
 	if (lseek(file_fd, 0, SEEK_SET) == -1) {
-		perror("[-] Error: Failed to seek input file to the beginning");
+		perror("[-] Error: Failed to seek input file to the beginning.");
 		return -1;
 	}
 
-	// Перемещаем указатель в конец хранилища для записи
 	if (lseek(storage_fd, 0, SEEK_END) == -1) {
-		perror("[-] Error: Failed to seek storage file to the end");
+		fprintf(stderr, "[-] Error: Failed to seek storage file to the end.");
 		return -1;
 	}
 
-	// Читаем данные из файла и записываем их в хранилище
 	while ((bytes_read = read(file_fd, buffer, BUFFER_SIZE)) > 0) {
 		bytes_written = write(storage_fd, buffer, bytes_read);
 		if (bytes_written == -1) {
-			perror("[-] Error: Failed to write data to storage file");
+			fprintf(stderr, "[-] Error: Failed to write data to storage file.");
 			return -1;
 		}
 		if (bytes_written != bytes_read) {
-			fprintf(stderr, "[-] Error: Incomplete write to storage file\n");
+			fprintf(stderr, "[-] Error: Incomplete write to storage file.\n");
 			return -1;
 		}
 	}
 
-	// Блокируем хранилище после записи файла
 	perms.l_type = F_WRLCK;
 	perms.l_whence = SEEK_END;
 	perms.l_start = 0;
 	perms.l_len = 0;
 
 	if (fcntl(storage_fd, F_SETLK, &perms) == -1) {
-		perror("[-] Error: Failed to lock the file storage");
+		fprintf(stderr, "[-] Error: Failed to lock the file storage.");
 		return -1;
 	}
 
-	printf("[+] Success: The file has been saved! Free space in the block: %lu bytes\n",
-	       block_size - storage_size);
-
+	fprintf(stdout, "[+] Success: The file has been saved! Free space in the block: %lu bytes.\n",
+	        block_size - storage_size);
 	return 0;
 }
